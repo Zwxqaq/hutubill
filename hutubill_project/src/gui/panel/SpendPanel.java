35c01cdfc8ca5dd15a2cca47e6fd743ea680ccea
Zwxqaq <603353840@qq.com>
2018-09-01 22:56:09 +0800

    for ticket #2

---
+++ hutubill_project/src/gui/panel/SpendPanel.java
@@ -0,0 +1,22 @@
+package gui.panel;
+
+import javax.swing.*;
+
+public class SpendPanel {
+    JLabel lMonthSpend = new JLabel("本月消费");
+    JLabel lTOdaySpend = new JLabel("今日消费");
+    JLabel lAvgSpendPerDay = new JLabel("日均消费");
+    JLabel lMonthLeft = new JLabel("本月剩余");
+    JLabel lDayAvgAvailable = new JLabel("日均可用");
+    JLabel lMonthLeftDay = new JLabel("距离月末");
+
+    JLabel vMonthSpend = new JLabel("￥2300");
+    JLabel vTOdaySpend = new JLabel("￥25");
+    JLabel vAvgSpendPerDay = new JLabel("￥120");
+    JLabel vMonthLeft = new JLabel("￥2084");
+    JLabel vDayAvgAvailable = new JLabel("￥389");
+    JLabel vMonthLeftDay = new JLabel("15天");
+
+    public SpendPanel() {
+    }
+}
